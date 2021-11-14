d24ed38928314c90b62ddf3137810a994112ce3e
lipeehsantook <89170141+lipeehsantook@users.noreply.github.com>
2021-11-14 18:36:16 -0300

    Delete programacao-imp directory

--- programacao-imp/Aula08/aula08.js
+++
@@ -1,9 +0,0 @@
-let idade = 18;
-if(idade>14
-    ){
-    console.log("voce e maior de idade");
-}else if (idade == 18){
-    console.log("voce esta aprendendo");
-}else{
-    console.log("voce nao e maior de idade");
-}
--- programacao-imp/Aula10/arrays.js
+++
@@ -1,6 +0,0 @@
-
-let lista = ['felipe', 27, false, 'lindo'];
-console.log(lista);
-
-let ltrLitsa = lista.join(" * ");
-console.log(ltrLitsa);
