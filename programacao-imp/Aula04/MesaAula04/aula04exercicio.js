cecacaab4b547cb7ac57bbaaa0a164ddacff39e4
lipeehsantook <89170141+lipeehsantook@users.noreply.github.com>
2021-11-14 18:36:37 -0300

    Add files via upload

---
+++ programacao-imp/Aula04/MesaAula04/aula04exercicio.js
@@ -0,0 +1,8 @@
+const nome = "Jose da Silva";
+let idade = 27;
+let peso = 55;
+let altura = 1.70;
+let plano = true;
+let imc = null;
+imc - peso (altura*altura);
+console.log( nome,"tem",idade,"anos e seu indice de massa corporal é",imc );
