f05d9db0f0f281f53fede9d464bdf36836432baa
lipeehsantook <89170141+lipeehsantook@users.noreply.github.com>
2021-11-27 02:21:26 -0300

    Add files via upload

---
+++ programacao-imp/Aula13/exercicioAula13.js
@@ -0,0 +1,45 @@
+let redeSocial = {
+"nome" : "Felipe",
+"apelido" : "Santos",
+"idade" : 26,
+"usuario" : "shuashuahsuh",
+"email" : "felipe-k-@hotmail.com"
+}
+
+console.log(redeSocial)
+
+
+// EXERCICIO 
+
+
+
+function Conta(nroConta, tipoConta, saldo, nomeCompleto){
+    this.nroConta = nroConta;
+    this.tipoConta = tipoConta;
+    this.saldo = saldo;
+    this.nomeCompleto = nomeCompleto;
+}
+
+let contaTeste = new Conta("0000000009", "Conta Corrente", 250000, "Danielle Alves")
+
+let conta1 = new Conta(5486273622, "Conta Corrente",27771,"Abigael Natte");
+let conta2 = new Conta(1183971869, "Conta Poupança",8675,"Ramon Connell");
+let conta3 = new Conta(9582019689,"Conta Poupança",27363,"Jarret Lafuente");
+let conta4 = new Conta(1761924656,"Conta Poupança",32415,"Ansel Ardley");
+let conta5 = new Conta(7401971607,"Conta Poupança",18789,"Jacki Shurmer");
+let conta6 = new Conta(630841470,"Conta Corrente",28776,"Jobi Mawtus");
+let conta7 = new Conta(4223508636,"Conta Corrente",2177,"Thomasin Latour");
+let conta8 = new Conta(185979521,"Conta Poupança",25994,"Lonnie Verheijden");
+let conta9 = new Conta(3151956165,"Conta Corrente",7601,"Alonso Wannan");
+let conta10 = new Conta(2138105881,"Conta Poupança",33196,"Bendite Huggett");
+
+let listaDeContas = [conta1, conta2, conta3, conta4, conta5, conta6, conta7, conta8, conta9, conta10]
+
+function banco(listaDeContas)
+
+console.log(banco);
+
+
+
+
+
